f787573f21d5249c4682fb5b789fd399a95c95d0
qzzloz <xlivsxd@gmail.com>
2024-04-04 16:04:24 +0900

    upload

---
+++ src/main/java/com/example/SSUCout/SsuCoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSUCout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuCoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuCoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSUCout/SsuCoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSUCout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuCoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
