5f1f3e17635a4f25ee4af96ba41c2cd9bf1e9b3f
qzzloz <xlivsxd@gmail.com>
2024-04-08 16:27:17 +0900

    temp

---
+++ src/main/java/com/example/SSUCout/club/ClubRepository.java
@@ -0,0 +1,8 @@
+package com.example.SSUCout.club;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepository extends JpaRepository<Club, Integer> {
+}
