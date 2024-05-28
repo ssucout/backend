986d7e35faf7f1cbb34b504d51d1ae0b7fa5ee44
minseo <kmseo0707@naver.com>
2024-05-28 21:50:55 +0900

    club/list/search 해결 및 Application.java 추가

--- src/main/java/com/example/SSUCout/club/ClubApiController.java
+++ src/main/java/com/example/SSUCout/club/ClubApiController.java
@@ -1,6 +1,7 @@
 package com.example.SSUCout.club;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,12 +10,21 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/club")
 public class ClubApiController {
+    @Autowired
     private final ClubService clubService;
 
     @GetMapping("/list")
     public List<ClubDTO> view(
     ){
         return clubService.listAll();
+    }
+
+    @GetMapping("/list/search")
+    public List<ClubDTO> searchClubs(
+            @RequestParam(name = "term")
+            String term
+    ){
+        return clubService.searchClubs(term);
     }
 
 
--- src/main/java/com/example/SSUCout/club/ClubDTO.java
+++ src/main/java/com/example/SSUCout/club/ClubDTO.java
@@ -1,7 +1,5 @@
 package com.example.SSUCout.club;
 
-
-import com.example.SSUCout.review.Review;
 import lombok.*;
 
 import java.util.List;
@@ -24,7 +22,7 @@ public class ClubDTO {
         dto.setClubId(club.getClubId());
         dto.setClubName(club.getClubName());
         dto.setClubCategory(club.getClubCategory());
-        dto.setTotalStar(calculateAverageStar(club.getReviewList()));
+        dto.setTotalStar(club.getTotalStar());
         return dto;
     }
 
@@ -32,13 +30,5 @@ public class ClubDTO {
         return clubs.stream()
                 .map(ClubDTO::toDTO)
                 .collect(Collectors.toList());
-    }
-
-    private static double calculateAverageStar(List<Review> reviews) {
-        if (reviews.isEmpty()) {
-            return 0.0;
-        }
-        double sum = reviews.stream().mapToDouble(Review::getUserStar).sum();
-        return sum / reviews.size();
     }
 }
